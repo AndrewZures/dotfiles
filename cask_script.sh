23b52215f307314aec916b50d9ddb168ccd601b9
Andrew Zures <azures@8thlight.com>
2015-02-21 20:43:38 -0600

    adds to cask script

---
+++ cask_script.sh
@@ -0,0 +1,23 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+
+brew install caskroom/cask/brew-cask
+
+brew cask install google-chrome
+brew cask install firefox
+
+brew cask install iterm2
+brew cask install intellij-idea-ce
+brew cask install mysqlworkbench
+
+brew cask install hipchat
+brew cask install skype
+brew cask install fuze
+
+brew cask install alfred
+brew cask install 1password
+brew cask install spectacle
+brew cask install evernote
+
+brew cask install spotify
+brew cask install airfoil
